d7e2b293e6d7590b8543b241b913a8c2f8ce4b0a
emisasal <emiliano.sasal@gmail.com>
2022-03-24 12:56:35 -0300

    cambios en userController

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,29 +1,34 @@
-const User = require('../models/Users')
+const User = require("../models/Users")
 
 exports.register = (req, res) => {
-    const { firstName, surname, age, country, email, password } = req.body;
-    User.create({
+  const { firstName, surname, age, country, email, password } = req.body
+  User.findAll({ where: { email: email } }).then(data => {
+    if (data[0]) res.status(400).send(false)
+    else {
+      User.create({
         firstName,
         surname,
         age,
         country,
         email,
         password,
-    }).then(user => {
-        res.status(201).send(user);
-    });
-};
+      })
+        .then(user => res.status(201).send(user))
+        .catch(() => res.status(404))
+    }
+  })
+}
 
 exports.login = (req, res) => {
-    res.send(req.user);
-};
+  res.send(req.user)
+}
 
 exports.me = (req, res) => {
-    if (!req.user) return res.sendStatus(401);
-    res.send(req.user);
-};
+  if (!req.user) return res.sendStatus(401)
+  res.send(req.user)
+}
 
 exports.logout = (req, res) => {
-    req.logOut();
-    res.sendStatus(200);
-};
+  req.logOut()
+  res.sendStatus(200)
+}
